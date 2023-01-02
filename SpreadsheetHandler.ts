b67167491cc11bf50a5e53b8f443860974dd2d85
David Veszelovszki <veszelovszki@gmail.com>
2023-01-02 19:28:23 +0100

    Remove dead code for adding a subscription
    Might need this later if I ever want to invest in this.

--- SpreadsheetHandler.ts
+++ SpreadsheetHandler.ts
@@ -7,7 +7,6 @@ export namespace SpreadsheetHandler {
     const nextArticlesSheet = spreadsheet.getSheetByName('Next')
     const logSheet = spreadsheet.getSheetByName('Log')
     const subscribersSheet = spreadsheet.getSheetByName('Subscribers')
-    const subscriptionsSheet = spreadsheet.getSheetByName('Subscriptions')
 
     export function loadArticles(count: number): Article[] {
         const rows = nextArticlesSheet.getRange(2, 1, count, 14).getValues()
@@ -31,47 +30,6 @@ export namespace SpreadsheetHandler {
         for (const [index, article] of articles.entries()) {
             logSheet.getRange(rowIndex + index, 1, 1, 2).setValues([[today, article.originalUrl]])
         }
-    }
-
-    // noinspection JSUnusedLocalSymbols
-    export function addSubscription(
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        emailAddress: string,
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        hungarianPosts: boolean,
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        englishPosts: boolean,
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        articleSrs: boolean
-    ): number {
-        // TODO: This is not done yet!
-        // I was here: https://developers.google.com/apps-script/reference/spreadsheet/sheet#appendRow(Object)
-        // And here: https://stackoverflow.com/questions/34689556/how-do-i-append-a-blank-row-in-a-google-spreadsheet-with-apps-script
-        // And here: https://developers.google.com/apps-script/guides/web
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const request: any = {}
-
-        // we'll assume header is in row 1, but you can override with header_row in GET/POST data
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
-        const headRow = request.parameter.header_row || 1
-        const headers = subscriptionsSheet.getRange(1, 1, 1, subscriptionsSheet.getLastColumn()).getValues()[0]
-        const nextRow = subscriptionsSheet.getLastRow() + 1 // get next row
-        const row = []
-        // loop through the header columns
-        for (const header of headers) {
-            if (header === 'Timestamp') {
-                // special case if you include a 'Timestamp' column
-                row.push(new Date())
-            } else {
-                // else use header name to get data
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-                row.push(request.parameter[header])
-            }
-        }
-        // more efficient to set values as [][] array than individually
-        subscriptionsSheet.getRange(nextRow, 1, 1, row.length).setValues([row])
-
-        return nextRow
     }
 
     function convertRowToArticle(row: string[]): Article | null {
